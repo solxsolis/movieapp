aa90dd01b177ffa325a57ced064af68407b6e060
solxsolis <anna.tamelo@gmail.com>
2025-09-15 15:50:55 +0200

    added search bar

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -5,6 +5,7 @@ import MovieCard from "./MovieCard";
 export default function MoviesGrid() {
 
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
 
     useEffect(() => {
@@ -13,15 +14,28 @@ export default function MoviesGrid() {
         .then(data => setMovies(data))
     }, []);
 
-    return (
-        <div className = 'movies-grid'> 
+    const handleSearchChange = (e) => {
+      setSearchTerm(e.target.value);
+    };
 
-        {
-            movies.map(movie => (
-                <MovieCard movie={movie} key={movie.id}></MovieCard>
-            ))
-        }
-            
+    const filteredMovies = movies.filter(movie =>
+      movie.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+    );
+
+    return (
+      <div>
+        <input 
+          type="text"
+          className="search-input"
+          placeholder="Search movies..."
+          value = {searchTerm}
+          onChange = {handleSearchChange}
+        />
+        <div className="movies-grid">
+          {filteredMovies.map((movie) => (
+            <MovieCard movie={movie} key={movie.id}></MovieCard>
+          ))}
         </div>
+      </div>
     );
 }
